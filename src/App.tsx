a2977d2760baef5ede1f9e0b328562291bdeb17a
shaikmalikbasha583 <shaikmalikbasha583@gmai.com>
2026-02-28 15:32:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>SM Dental care & Implant center</h1>
+      <p>Dental clinic in Narasaraopet, Andhra Pradesh</p>
+    </div>
+  );
+}
